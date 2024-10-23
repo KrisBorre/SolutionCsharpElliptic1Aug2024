ef8acbc58f9dfac9e35230cfc1f3563107248240
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-10-23 12:22:59 +0200

    Butcher (2008)

--- LibraryDifferentialEquationsButcherExercise20point2_2Sep2024/DifferentialEquationsButcherExercise20point2_2Sep2024.cs
+++ LibraryDifferentialEquationsButcherExercise20point2_2Sep2024/DifferentialEquationsButcherExercise20point2_2Sep2024.cs
@@ -3,9 +3,16 @@ using System.Numerics;
 
 namespace LibraryDifferentialEquationsButcherExercise20point2_2Sep2024
 {
+    /// <summary>
+    /// Butcher (2008) page 65 Exercise 20.2
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class DifferentialEquationsButcherExercise20point2_2Sep2024<T> : DifferentialEquationsBaseClass26feb2024<T>
        where T : INumber<T>
     {
+        /// <summary>
+        /// Butcher (2008) page 65 Exercise 20.2
+        /// </summary>
         public DifferentialEquationsButcherExercise20point2_2Sep2024()
         {
             this.NumberOfFirstOrderEquations = 2;
